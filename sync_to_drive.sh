1f529abe7115602b6511e6d3bb2d6bad5c0cb331
too-to <140312995+too-to@users.noreply.github.com>
2025-07-21 11:25:42 +0900

    Update sync_to_drive.sh

--- sync_to_drive.sh
+++ sync_to_drive.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-SRC=~/features                 # 피처 파일이 저장되는 로컬 경로
-DST=~/mydrive/features         # Google Drive에 연결된 디렉토리 (rclone mount한 곳)
-mkdir -p "$DST"                # 대상 폴더가 없으면 만든다
+SRC=~/features
+DST=gdrive:feature
+LOG=~/logs/drive_sync.log
 
-rclone copy "$SRC" "$DST" \
-  --update \                   # 변경된 파일만 복사
-  --log-file=~/logs/drive_sync.log   # 로그 파일로 저장
+mkdir -p "$SRC"
+mkdir -p ~/logs
+
+echo "[$(date)] Sync start" >> "$LOG"
+rclone copy "$SRC" "$DST" --update --log-file="$LOG"
+echo "[$(date)] Sync complete" >> "$LOG"
+
